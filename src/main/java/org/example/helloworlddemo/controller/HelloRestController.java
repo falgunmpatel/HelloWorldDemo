63ce97d84adc9166eca535398f861f080b952f56
Falgun Patel <falgunmpatel9123@gmail.com>
2025-03-01 14:09:56 +0530

    [Falgun Patel] uc2 - Create Hello REST Controller
    Signed-off-by: Falgun Patel <falgunmpatel9123@gmail.com>

---
+++ src/main/java/org/example/helloworlddemo/controller/HelloRestController.java
@@ -0,0 +1,12 @@
+package org.example.helloworlddemo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloRestController {
+  @RequestMapping("/")
+  public String index() {
+    return "Hello from BridgeLabz";
+  }
+}
